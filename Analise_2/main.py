3d891207ca740c2f32a891364662fa58095ce779
Gustavo-Murilo <gustavomurilo012@gmail.com>
2023-10-14 22:32:45 -0300

    atualização nos nomes de variáveis

--- Analise_2/main.py
+++ Analise_2/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 import Analise_2.tratamento_dados as td
-import Analise_2.vizualizacao as viz
+import Analise_2.visualizacao as vis
 
 # Carregamento da bases de dados sobre febre amarela
 df = td.carregar_dataframes()
@@ -11,12 +11,12 @@ df_datas = td.organizar_df_datas(df)
 
 # PERGUNTA 2-a): Qual a relação entre os anos e a quantidade de infecções e ou mortes?
 ocorrencias_por_ano = td.organizar_df_ano(df_datas)
-viz.plotar_ocorrencias_ano(ocorrencias_por_ano)
+vis.plotar_ocorrencias_ano(ocorrencias_por_ano)
 
 # PERGUNTA 2-b) No geral, há algum mês do ano com mais infecções?
-df_meses = td.organizar_df_mes(df_datas)
-viz.plotar_mortes_por_mes(df_meses)
+ocorrencias_por_mes = td.organizar_df_mes(df_datas)
+vis.plotar_mortes_por_mes(ocorrencias_por_mes)
 
 # PERGUNTA 2-c) Há alguma relação entre a letalidade da doença e o passar do tempo?
-df_letalidade = td.organizar_df_letalidade(ocorrencias_por_ano)
-viz.plotar_letalidade(df_letalidade)
+letalidade_por_ano = td.organizar_df_letalidade(ocorrencias_por_ano)
+vis.plotar_letalidade(letalidade_por_ano)
